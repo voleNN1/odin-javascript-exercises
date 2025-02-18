729546cda6ba270532257e9618127ead9cfe6f3c
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-18 00:44:01 -0500

    Complete exercise 1, Attempt exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(repeatThis, repeatTimes) {
+    for (i = 0; i <= repeatTimes; i++) {
+        repeatThis
+    }
+    return console.log(repeatThis);
 };
 
 // Do not edit below this line
