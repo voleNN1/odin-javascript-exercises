a8b0f1e20fc1f02a81c3c52ffa51fd4bdaadea0a
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-19 00:55:09 -0500

    Complete step 1 of exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let wordString = "";
+    wordString = word;
+    let wordArray = wordString.split("");
+    wordArray.reverse();
+    let finalString = wordArray.join("");
 
+    return finalString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
