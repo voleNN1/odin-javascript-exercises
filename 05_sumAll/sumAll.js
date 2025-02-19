be9c510f3be2d2a46f9e0be2486233994d7b63e5
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-19 13:36:40 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let finalInt = 0;
 
+    if (firstInt < 0 || secondInt < 0) {
+        finalInt = "ERROR";
+    }
+    else if (firstInt % 1 !== 0 || secondInt % 1 !== 0) {
+        finalInt = "ERROR";
+    }
+    else if (Number.isInteger(firstInt) != true || Number.isInteger(secondInt) != true) {
+        finalInt = "ERROR";
+    }
+    else if (firstInt < secondInt) {
+        for (i = firstInt; i <= secondInt; i++) {
+            finalInt = finalInt + i;
+        }
+    }
+    else if (firstInt > secondInt) {
+        for (i = secondInt; i <= firstInt; i++) {
+            finalInt = finalInt + i;
+        }
+    }
+
+
+    return finalInt;
 };
 
 // Do not edit below this line
