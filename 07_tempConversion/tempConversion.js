9b500876eb4ff05b8f6dfabdf4caa5968ca0c2b8
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-19 21:38:08 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(cTemp) {
+  let fTemp = ((cTemp - 32) * (5/9));
+  fTemp = Math.round(fTemp * 10) / 10;
+  return fTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fTemp) {
+  let cTemp = ((fTemp * 1.8) + 32);
+  cTemp = Math.round(cTemp * 10) / 10;
+  return cTemp;
 };
 
 // Do not edit below this line
