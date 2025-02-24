57804eeb9b1cdf7a93854c88647c68c537b33b2f
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-23 23:44:42 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+  let total = numOne + numTwo;
+
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	let total = numOne - numTwo;
+
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(values) {
+	let total = 0;
+
+  for (i = 0; i < values.length; i++) {
+    total += values[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(values) {
+  let total = 1;
 
+  for (i = 0; i < values.length; i++) {
+    total = (total * values[i]);
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(numOne, numTwo) {
+	let total = numOne ** numTwo;
+
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	if (value === 0 || value === 1) {
+    return 1;
+  }
+  else {
+    return value * factorial(value-1);
+  }
 };
 
 // Do not edit below this line
