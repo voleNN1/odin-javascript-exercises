c92fe55297a1e75a3ecc669998225ed03daa666f
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-24 00:05:29 -0500

    Start progress on exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let strippedText = text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    console.log(strippedText);
+    if (strippedText === reverseText(text)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+function reverseText(text) {
+    let lowercaseText = text.toLowerCase();
+    return lowercaseText.split("").reverse().join("");
+}
+
+palindromes("!racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
