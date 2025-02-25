a72f4902f4f650725946fefdab94dcf071a416cd
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-24 19:14:37 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(numToFind) {
+    numToFind = parseInt(numToFind);
+    if (numToFind < 0) {
+        return "OOPS";
+    }
+    else if (numToFind <= 1) {
+        return numToFind;
+    }
+    else {
+        let finalNum = fibonacci(numToFind - 1) + fibonacci(numToFind - 2);
 
+        return finalNum;
+    }
 };
 
 // Do not edit below this line
