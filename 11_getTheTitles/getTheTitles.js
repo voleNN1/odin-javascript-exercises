956694456b55c7d6aac56ea9ccef37747536c821
Josh McCallister <196588493+voleNN1@users.noreply.github.com>
2025-02-24 20:00:17 -0500

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(list) {
+    let titles = list.map(item => item.title);
+    
+    return titles;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
